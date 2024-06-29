d747b87fc226e685d4bbf134e624537d2d052bd3
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-06-29 20:24:53 +0000

    rt

--- Hello.py
+++ Hello.py
@@ -257,4 +257,4 @@ if c == "longeur d'un text":
   container11.write(f"la longeur d texte que vous avez entré est : {len(text)}")
 
 st.write("si vous voulez me soutenir clicker sur ce lien (vous pouvez clicker puis fermer la page imediatement):")
-sT.write("[le lien](https://www.highrevenuenetwork.com/tg3snhn54z?key=eef39f115cc96111cef42bcfa506ff6b)")
+st.write("[le lien](https://www.highrevenuenetwork.com/tg3snhn54z?key=eef39f115cc96111cef42bcfa506ff6b)")
